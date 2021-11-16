a215aa646e70cf9eb870606ac9773d8074bb3d81
pierredeneuville <deneuville.pierre@gmail.com>
2021-11-16 12:33:59 +0100

    saving routes and actions

--- app/controllers/offres_controller.rb
+++ app/controllers/offres_controller.rb
@@ -1,2 +1,21 @@
 class OffresController < ApplicationController
+  def new
+    @offre = Offre.new
+  end
+
+  def create
+    @offre = Offre.new(offre_params)
+    @offre.user = @user
+    if @offre.save
+      redirect_to offre_path(@offre)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def offre_params
+    params.require(:offre).permit(:métier, :prix)
+  end
 end
